2642965e42199947d32d96bf31ae4b8e4647c91e
Julien Atanassian <julien.atanassian@gmail.com>
2019-09-02 21:42:09 +0000

    Added a POST route to receive the form submission and added a function that generates random strings

--- express_server.js
+++ express_server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(6);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +35,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
